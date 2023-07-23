aeb094e5b083be27591de5ed3d3bbdae8576a642
Kavinsk02 <66780940+Kavinsk02@users.noreply.github.com>
2023-07-23 10:13:11 +0530

    Add files via upload

---
+++ Changepassword.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication13.Repository
+{
+    public class Changepassword
+    {
+        string conString = ConfigurationManager.ConnectionStrings["DBconnect"].ToString();
+
+        public object YourPasswordHashingFunction { get; private set; }
+
+        public bool ChangePassword(string username, string newPassword)
+        {
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                string query = "UPDATE Signup SET Password = @NewPassword WHERE Username = @Username";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@NewPassword", newPassword);
+                    command.Parameters.AddWithValue("@Username", username);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+        
+
+
+    }
+}
---
+++ ChnagepasswordController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication13.Models;
+using System.Configuration;
+
+namespace WebApplication13.Controllers
+{
+    public class ChnagepasswordController : Controller
+    {
+        string conString = ConfigurationManager.ConnectionStrings["DBconnect"].ToString();
+        Changepassword changepassword=new Changepassword();
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(Changepassword cpmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cpmodel);
+            }
+
+            if (!IsOldPasswordValid(User.Identity.Name, cpmodel.OldPassword))
+            {
+                ModelState.AddModelError("OldPassword", "The old password is incorrect.");
+                return View(cpmodel);
+            }
+
+            bool passwordChanged = changepassword.ChangePassword(User.Identity.Name, cpmodel.NewPassword);
+            if (passwordChanged)
+            {
+                return RedirectToAction("ChangePasswordSuccess");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Failed to change the password. Please try again.");
+                return View(cpmodel);
+            }
+        }
+
+
+        public ActionResult ChangePasswordSuccess()
+        {
+            return View();
+        }
+
+        private bool IsOldPasswordValid(string username, string oldPassword)
+        {
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                string query = "SELECT Password FROM Signup WHERE Username = @Username";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+
+                    connection.Open();
+                    string storedPassword = command.ExecuteScalar()?.ToString();
+
+
+                    bool passwordIsValid = (oldPassword == storedPassword);
+                    return passwordIsValid;
+                }
+            }
+        }
+    }
+}
